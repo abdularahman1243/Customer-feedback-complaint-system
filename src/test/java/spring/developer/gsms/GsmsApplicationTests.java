87862037ba631605041683f61fcaf9243b9398bf
Abdul Rahman 'Bahadurzai <abdulrahmanbahadori@gmail.com>
2025-12-18 17:35:39 +0430

    Initial project setup

---
+++ src/test/java/spring/developer/gsms/GsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.developer.gsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
